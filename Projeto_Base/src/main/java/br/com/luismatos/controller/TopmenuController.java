d1e4d0174bef9a43ac3446d05bdb49498e4afc9f
Trabalho <luimatos>
2018-01-17 20:54:12 -0200

    atualizando projeto, inicio implementação para controle de perfil de usuarios/controle de acessos

---
+++ Projeto_Base/src/main/java/br/com/luismatos/controller/TopmenuController.java
@@ -0,0 +1,16 @@
+package br.com.luismatos.controller;
+
+import java.io.Serializable;
+
+public class TopmenuController implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4296968999226392269L;
+	
+	
+	
+	
+
+}
