b835a374e27b291c724776db4f8cae5445f22fd3
Trabalho <luimatos>
2018-01-29 15:46:12 -0200

    Atualizando topmenu com dropmenu submenu e começando a implementar o cadastro de novo usuario

--- Projeto_Base/src/main/java/br/com/luismatos/controller/TopmenuController.java
+++ Projeto_Base/src/main/java/br/com/luismatos/controller/TopmenuController.java
@@ -1,16 +1,25 @@
 package br.com.luismatos.controller;
 
+import java.io.IOException;
 import java.io.Serializable;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
 public class TopmenuController implements Serializable{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -4296968999226392269L;
-	
-	
-	
-	
+
+
+	public void redirecionaPaginaNovoUsuario() throws IOException{
+		FacesContext.getCurrentInstance().getExternalContext().redirect("novoUsuario");
+	}
 
 }
